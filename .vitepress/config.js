5eb50fd86e7d30f57520a5eb51145d508946f50f
Olivier Colas <olivercolas.oc@gmail.com>
2022-02-18 20:05:33 +0100

    Initial Commit

---
+++ .vitepress/config.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: '<Project_name>',
+      description: '🛠️ A description here'
+    }
+  },
+
+  themeConfig: {
+    repo: '',
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: 'Edit this page on Github',
+    lastUpdated: 'Last Updated',
+
+    nav: [
+      { text: 'Guide', link: '/guide/' }
+    ],
+
+    sidebar: {
+      '/guide/': [
+        {
+          text: 'Overview',
+          link: '/guide/',
+          collapsable: true
+        },
+
+        {
+          text: 'Installation',
+          link: '/guide/installation'
+        },
+      ]
+    }
+  },
+
+  markdown: {
+    lineNumbers: true
+  }
+}
---
+++ index.md
@@ -0,0 +1,15 @@
+---
+home: true 
+heroImage: /logo.svg 
+actionText: Get started → 
+actionLink: /guide/
+---
+
+## My Docs Title Here
+
+Vitepress is awesome!
+
+```bash
+yarn add --dev vitepress
+```
+
